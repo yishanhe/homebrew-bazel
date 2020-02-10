8eff86226dcc0080f82b83336c72ef3f86984564
Shanhe Yi <yshanhe@vmware.com>
2020-02-10 00:00:29 -0800

    auto update 2.1.0 2.0.0 1.2.1 1.2.0 1.1.0 1.0.1 1.0.0 0.29.1 0.29.0 0.28.1

---
+++ Formula/bazel@2.1.0.rb
@@ -0,0 +1,39 @@
+class BazelAT210 < Formula
+    desc "Fast, scalable, multi-language and extensible build system"
+    homepage "https://bazel.build/"
+    url "https://releases.bazel.build/2.1.0/release/bazel-2.1.0-installer-darwin-x86_64.sh", :using => :nounzip
+    version "2.1.0"
+    sha256 "0be7e8266523922ab63720775a6e4174468bd617e016e89d726a0f3441c46396"
+
+    bottle :unneeded
+
+    def install
+        chmod 0555, "bazel-#{version}-installer-darwin-x86_64.sh"
+        system "./bazel-#{version}-installer-darwin-x86_64.sh", "--prefix=#{buildpath}"
+        bin.install "lib/bazel/bin/bazel" => "bazel"
+        bin.install "lib/bazel/bin/bazel-real" => "bazel-real"
+        bash_completion.install "lib/bazel/bin/bazel-complete.bash"
+        zsh_completion.install "lib/bazel/bin/_bazel"
+    end
+
+
+    test do
+        touch testpath/"WORKSPACE"
+        (testpath/"Main.java").write <<~EOS
+          public class Main {
+            public static void main(String... args) {
+              System.out.println("Hello world!");
+            }
+          }
+        EOS
+        (testpath/"BUILD").write <<~EOS
+          java_binary(
+            name = "main",
+            srcs = ["Main.java"],
+            main_class = "Main",
+          )
+        EOS
+        system bin/"bazel", "build", "//:main"
+        system "bazel-bin/main"
+    end
+end
